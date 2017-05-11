1436d56c57c554bf3f1ab5a77663a0887a3d8892
K.Bailey <katebailey1230@gmail.com>
2017-05-11 15:09:48 -0400

    create from command line

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,13 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
 
      resources :users do
-       resources :lists, only: [:create, :update, :destroy]
+       resources :lists
      end
+
+     resources :lists, only: [] do
+       resources :items, only: [:create]
+     end
+
+     resources :items, only: [:destroy]
    end
 end
